3cca3003873ce02296dc5f9d54e8827421c036e6
Brian Harring <ferringb@gentoo.org>
2005-08-25 03:58:27 +0000

    header changes ($Header$ -> $Id$), and making the tag in ebuild-daemon.lib commented again
    svn path=/main/branches/savior/; revision=1913

--- bin/ebuild-env/ebuild-daemon.sh
+++ bin/ebuild-env/ebuild-daemon.sh
@@ -2,7 +2,7 @@
 # ebuild-daemon.sh; core ebuild processor handling code
 # Copyright 2004 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /home/cvsrep/bharring/portage/bin/ebuild-daemon.sh,v 1.4 2005/07/07 05:28:27 bharring Exp $
+# $Id$
 
 alias die='diefunc "$FUNCNAME" "$LINENO" "$?"'
 #alias listen='read -u 3 -t 10'
