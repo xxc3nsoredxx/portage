0332c952c4a225ad7c13b3e564f47b3c30637a5c
Brian Harring <ferringb@gentoo.org>
2005-09-20 23:40:33 +0000

    safe import; threading.Lock used to insure that portage code/plugins don't get crap imports that get left in sys.modules from a previous failed import (go python :/)
    svn path=/main/branches/savior/; revision=2016

--- portage/util/modules.py
+++ portage/util/modules.py
@@ -3,24 +3,39 @@
 # License: GPL2
 # $Id$
 
+import sys, threading
+
 class FailedImport(ImportError):
 	def __init__(self, trg, e):	self.trg, self.e = trg, e
 	def __str__(self):	return "Failed importing target '%s': '%s'" % (self.trg, self.e)
 
+__import_lock = threading.Lock()
+
 def load_module(name):
 	"""load a module, throwing a FailedImport if __import__ fails"""
+	__import_lock.acquire()
+	if name in sys.modules:
+		return sys.modules[name]
 	try:
-		m = __import__(name)
-		nl = name.split('.')
-		# __import__ returns nl[0]... so.
-		nl.pop(0)
-		while len(nl):
-			m = getattr(m, nl[0])
+		try:
+			m = __import__(name)
+			nl = name.split('.')
+			# __import__ returns nl[0]... so.
 			nl.pop(0)
-		return m	
-	except (AttributeError, ImportError), e:
-		raise FailedImport(name, e)
-	
+			while len(nl):
+				m = getattr(m, nl[0])
+				nl.pop(0)
+			return m	
+		except AttributeError, e:
+			raise FailedImport(name, e)
+		except ImportError, e:
+			try:
+				del sys.modules[name]
+			except KeyError:
+				pass
+			raise FailedImport(name, e)
+	finally:
+		__import_lock.release()
 
 def load_attribute(name):
 	"""load a specific attribute, rather then a module"""
