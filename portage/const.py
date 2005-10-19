4804d1a703fc656b218efd5112f2b0f7a816107b
Brian Harring <ferringb@gentoo.org>
2005-10-19 09:30:14 +0000

    missed the plugins_dir constant commit.
    svn path=/main/branches/savior/; revision=2146

--- portage/const.py
+++ portage/const.py
@@ -60,3 +60,4 @@ RSYNC_BIN               = "/usr/bin/rsync"
 RSYNC_HOST              = "rsync.gentoo.org/gentoo-portage"
 
 CVS_BIN                 = "/usr/bin/cvs"
+plugins_dir				= "/var/lib/portage/plugins/"
