09d221b46548cef8bd48259aeab70af274d213ea
Brian Harring <ferringb@gentoo.org>
2005-10-20 13:02:01 +0000

    via marienz, func trickery to bind doc/module attributes to curried funcs (decorated is proper term), so that it appears normal under pydoc
    svn path=/main/branches/savior/; revision=2153

--- portage/util/currying.py
+++ portage/util/currying.py
@@ -3,19 +3,34 @@
 # License: GPL2
 # $Id$
 
-def pre_curry(*args, **kwargs):
+def pre_curry(func, *args, **kwargs):
 	"""passed in args are prefixed, with further args appended"""
+
 	def callit(*moreargs, **morekwargs):
 		kw = kwargs.copy()
 		kw.update(morekwargs)
-		return args[0](*(args[1:]+moreargs), **kw)
+		return func(*(args+moreargs), **kw)
+
+	callit.__original__ = func
 	return callit
 
-def post_curry(*args, **kwargs):
+def post_curry(func, *args, **kwargs):
 	"""passed in args are appended to any further args supplied"""
+
 	def callit(*moreargs, **morekwargs):
 		kw = morekwargs.copy()
 		kw.update(kwargs)
-		return args[0](*(moreargs+args[1:]), **kw)
+		return func(*(moreargs+args), **kw)
+
+	callit.__original__ = func
 	return callit
+
+def pretty_docs(wrapped, extradocs=None):
+	wrapped.__module__ = wrapped.__original__.__module__	    
+	doc = wrapped.__original__.__doc__
+	if extradocs is None:
+		wrapped.__doc__ = doc
+	else:
+		wrapped.__doc__ = extradocs
+	return wrapped
 
