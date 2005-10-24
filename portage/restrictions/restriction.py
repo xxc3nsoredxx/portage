f91cbefff96c7b27415ec73a5747205a65fb7e60
Brian Harring <ferringb@gentoo.org>
2005-10-24 18:11:12 +0000

    ongoing fixups from marienz
    svn path=/main/branches/savior/; revision=2162

--- portage/restrictions/restriction.py
+++ portage/restrictions/restriction.py
@@ -3,13 +3,12 @@
 # License: GPL2
 # $Id$
 
-import re, logging
 
 class base(object):
 	"""base restriction matching object; overrides setattr to provide the usual write once trickery
 	all derivatives *must* be __slot__ based"""
 
-	__slots__ = ["negate"]
+	__slots__ = ["negate", "_hash"]
 	package_matching = False
 
 	def __init__(self, negate=False):
@@ -47,13 +46,17 @@ class base(object):
 	def __repr__(self):
 		return str(self)
 
+	def __str__(self):
+		# without this __repr__ recurses...
+		raise NotImplementedError
+
 	def __hash__(self):
 		# XXX: This likely isn't actually unique. Something is needed
 		# to uniquely identify restrictions though otherwise the object
 		# pointer is used.
 		# -- jstubbs
-		if "_hash" not in self.__dict__:
-			self.__dict__["_hash"] = hash(str(self))
+		if not hasattr(self, '_hash'):
+			self._hash = hash(str(self))
 		return self._hash
 
 class AlwaysBoolMatch(base):
