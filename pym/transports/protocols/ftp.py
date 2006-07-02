555bb449e865071695e35e75723caf273cf8dc53
Marius Mauch <genone@gentoo.org>
2006-07-02 21:16:55 +0000

    add http and ftp protocols
    svn path=/private/genone/branches/portage-modular-sync/; revision=3764

---
+++ pym/transports/protocols/ftp.py
@@ -0,0 +1,8 @@
+# Copyright 1998-2004 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+# $Id: portage_const.py 3761 2006-07-02 19:40:56Z genone $
+
+from transports.protocols import Protocol
+
+protocol = Protocol("ftp")
+Protocol.register(protocol)
