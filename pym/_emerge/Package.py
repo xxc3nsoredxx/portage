5077a3273c1f6224b1e954b1cd0063d97f383c86
Thomas Sachau <tommy@gentoo.org>
2012-02-26 14:02:10 +0100

    Package.py: Move multilib behind force-multilib FEATURE

--- pym/_emerge/Package.py
+++ pym/_emerge/Package.py
@@ -638,8 +638,9 @@ class _PackageMetadataWrapper(_PackageMetadataWrapperBase):
 		self._pkg.inherited = v
 
 	def _set_iuse(self, k, v):
-		for multilib_abis in self._pkg.root_config.settings.get("MULTILIB_ABIS", '').split(' '):
-			v = v + " multilib_abi_" + multilib_abis
+		if 'force-multilib' in self._pkg.root_config.settings.get("FEATURES", ''):
+			for multilib_abis in self._pkg.root_config.settings.get("MULTILIB_ABIS", '').split(' '):
+				v = v + " multilib_abi_" + multilib_abis
 		self._pkg.iuse = self._pkg._iuse(
 			v.split(), self._pkg.root_config.settings._iuse_implicit_match)
 
