c13439f0d8979387d42ebfa6eaff9799f41918c4
Thomas Sachau <tommy@gentoo.org>
2012-10-22 11:43:51 +0200

    Remove random character

--- pym/_emerge/actions.py
+++ pym/_emerge/actions.py
@@ -1,4 +1,4 @@
-w# Copyright 1999-2012 Gentoo Foundation
+# Copyright 1999-2012 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 from __future__ import print_function
