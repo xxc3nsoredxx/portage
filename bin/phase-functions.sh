1a35bcc3be9db6a2bd5eb50af19965b2b6950701
Nathan Phillip Brink <binki@gentoo.org>
2012-12-20 04:35:23 +0000

    Remove multilib workaround which saves .la files for media-libs/gst-plugins-* packages because bug #300256 has been resolved.

--- bin/phase-functions.sh
+++ bin/phase-functions.sh
@@ -675,9 +675,6 @@ __dyn_install() {
 			if [[ ${CATEGORY}/${PN} == sys-devel/libtool ]] ; then
 				ewarn "Preserving libltdl.la because of extensive usage"
 				ewarn "even in m4 macros of libtool"
-			elif [[ ${CATEGORY}/${PN} == media-libs/gst-plugins-* ]] ; then
-				ewarn "Preserving .la files for gst plugins"
-				ewarn "because of broken Gentoo package"
 			else
 				find "${D}" -name '*.la' ! -exec grep -q shouldnotlink=yes {} \; -exec rm {} \;
 			fi
