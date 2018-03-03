7db8c8d9e4581031be66896392571f989f3d3a9e
Thomas Sachau <tommy@gentoo.org>
2018-03-03 16:17:08 +0100

    Fix quoting (did leave some files in the builddir)

--- bin/phase-functions.sh
+++ bin/phase-functions.sh
@@ -306,7 +306,7 @@ __dyn_clean() {
 			"$PORTAGE_BUILDDIR"/.ipc_{in,out,lock} \
 			"$PORTAGE_BUILDDIR"/.exit_status
 
-		rm -rf "${PORTAGE_BUILDDIR}/{build-info,abi-code}"
+		rm -rf "${PORTAGE_BUILDDIR}"/{build-info,abi-code}
 		rm -rf "${WORKDIR}"
 		rm -f "${PORTAGE_BUILDDIR}/files"
 	fi
